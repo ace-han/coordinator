697f4271581ec643bb453ad75dbe081b9ed2bc02
Ace Han <ace.jl.han@gmail.com>
2014-05-30 01:49:34 +0800

    adopt i18n feature to this project

---
+++ src/main/java/org/jenkinsci/plugins/coordinator/model/CoordinatorBuild.java
@@ -0,0 +1,17 @@
+package org.jenkinsci.plugins.coordinator.model;
+
+import java.io.File;
+import java.io.IOException;
+
+import hudson.model.Build;
+
+public class CoordinatorBuild extends Build<CoordinatorProject, CoordinatorBuild> {
+
+	protected CoordinatorBuild(CoordinatorProject project) throws IOException {
+		super(project);
+	}
+
+	public CoordinatorBuild(CoordinatorProject project, File buildDir) throws IOException {
+        super(project, buildDir);
+    }
+}
--- src/main/java/org/jenkinsci/plugins/coordinator/model/CoordinatorProject.java
+++ src/main/java/org/jenkinsci/plugins/coordinator/model/CoordinatorProject.java
@@ -1,5 +1,52 @@
 package org.jenkinsci.plugins.coordinator.model;
 
-public class CoordinatorProject {
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
+import hudson.Extension;
+import hudson.model.ItemGroup;
+import hudson.model.TopLevelItem;
+import hudson.model.Project;
+import jenkins.model.Jenkins;
+
+@SuppressWarnings({ "unchecked" })
+public class CoordinatorProject extends
+		Project<CoordinatorProject, CoordinatorBuild> implements TopLevelItem {
+	
+	public CoordinatorProject(ItemGroup<?> parent, String name) {
+		super(parent, name);
+	}
+
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl) Jenkins.getInstance().getDescriptorOrDie(getClass());
+	}
+
+	@Override
+	protected Class<CoordinatorBuild> getBuildClass() {
+		return CoordinatorBuild.class;
+	}
+	
+	@Restricted(NoExternalUse.class)
+	@Extension(ordinal = 1000)
+	public static class DescriptorImpl extends AbstractProjectDescriptor {
+
+		/**
+		 * it shows like when create a new job the title line
+		 */
+		@Override
+		public String getDisplayName() {
+			return "Coordinator Project";
+		}
+
+		@Override
+		public CoordinatorProject newInstance(
+				@SuppressWarnings("rawtypes") ItemGroup parent, String name) {
+			// it's invoked by Jenkins#createProject#newInstance, parent=Hudson,
+			// name=whatever Job Name on the create interfaces
+			return new CoordinatorProject(parent, name);
+		}
+	}
+
 
 }
